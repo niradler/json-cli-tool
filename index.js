783757c8dc06848feef216dff55f8277440628ed
Nir Adler <github@niradler.com>
2020-02-01 20:21:39 +0200

    output and helpers

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ const set = require("lodash.set");
 const querystring = require("querystring");
 var _data = "";
 
+const output = (obj, type, limit) => {
+  if (limit && Array.isArray(obj)) {
+    obj = obj.slice(0, Number(limit));
+  }
+  switch (type) {
+    case "table":
+      console.table(obj);
+      break;
+    case "log":
+      console.log(obj);
+      break;
+    case "stringify":
+      console.log(JSON.stringify(obj));
+      break;
+    default:
+      console.log(obj);
+      break;
+  }
+};
+
 const _filter = (obj, term) => {
   return obj.filter(o => {
     let q = {};
@@ -73,10 +93,30 @@ function withPipe(data) {
     _data = json;
     const argv = minimist(process.argv.slice(2));
     let obj = json;
+    const jsonFormat = argv.output || argv.o;
+    const limit = argv.limit || argv.l;
+
     if (argv.path || argv.p) {
       obj = get(json, argv.path || argv.p, undefined);
     }
-    if (typeof obj != "object") console.log(JSON.stringify(obj));
+
+    if (argv["_"].length == 1) {
+      const cmd = argv["_"][0];
+      switch (cmd) {
+        case "keys":
+          console.log(Object.keys(obj));
+          break;
+        case "values":
+          console.log(Object.values(obj));
+          break;
+        default:
+          console.error("command not found.");
+          break;
+      }
+      return;
+    }
+
+    if (typeof obj != "object") output(obj, jsonFormat, limit);
     else {
       if (argv.map || argv.m) {
         let p = argv.map || argv.m;
@@ -88,7 +128,7 @@ function withPipe(data) {
       }
     }
     if (obj.length && obj.length == 1) obj = obj[0];
-    console.log({ obj, json, params: argv });
+    output(obj, jsonFormat, limit);
   } catch (error) {
     console.error("couldn't parsed: " + error.message);
   }
